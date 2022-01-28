12ae2b6de887ae2b2bbc65c8bcf98d74de6c4637
satrajit-chatterjee <satrajitdchatterjee@gmail.com>
2022-01-28 10:49:11 -0500

    cell decomposition:

--- src/lawn_mower_pattern.cpp
+++ src/lawn_mower_pattern.cpp
@@ -166,6 +166,254 @@ void ca::LawnMowerPattern::GeneratePath(double box_size_x, double box_size_y, do
     }
 }
 
+int ca::LawnMowerPattern::x_intersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+{
+    int num = (x1*y2 - y1*x2) * (x3-x4) -
+              (x1-x2) * (x3*y4 - y3*x4);
+    int den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4);
+    return num/den;
+}
+  
+// Returns y-value of point of intersection of
+// two lines
+int ca::LawnMowerPattern::y_intersect(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
+{
+    int num = (x1*y2 - y1*x2) * (y3-y4) -
+              (y1-y2) * (x3*y4 - y3*x4);
+    int den = (x1-x2) * (y3-y4) - (y1-y2) * (x3-x4);
+    return num/den;
+}
+
+std::vector<std::vector<double>> ca::LawnMowerPattern::intersectBoundary(std::vector<std::vector<double>> boundary_polygon, std::vector<double> coord){
+    std::vector<std::vector<double>> intersects;
+    for (int i=0; i<boundary_polygon.size(); ++i)
+    {
+        double a_val = boundary_polygon[i][1] - coord[1] - M_PI_2 * (boundary_polygon[i][0] - coord[0]);
+        double b_val = boundary_polygon[(i+1) % boundary_polygon.size()][1] - coord[1] - M_PI_2 * (boundary_polygon[(i+1) % boundary_polygon.size()][0] - coord[0]);
+        // if intersect
+        if (a_val/abs(a_val) != b_val/abs(b_val))
+        {
+            // find slope of edge
+            double m = boundary_polygon[(i+1) % boundary_polygon.size()][1] - boundary_polygon[i][1] / (boundary_polygon[(i+1) % boundary_polygon.size()][0] - boundary_polygon[i][0]);
+            intersects.push_back(
+                {
+                    (- (coord[1] - M_PI_2 * coord[0]) + (boundary_polygon[i][1] - m*boundary_polygon[i][0])) / (- m + M_PI_2), 
+                    ((boundary_polygon[i][1] - m*boundary_polygon[i][0]) * M_PI_2 - (coord[1] - M_PI_2 * coord[0]) * m) / (- m + M_PI_2),
+                    -1  // -1 denotes boundary intercept
+                }
+            );   
+        }
+    }
+    return intersects;
+}
+
+void ca::LawnMowerPattern::clip(std::vector<std::vector<double>> poly_points, int poly_size, double x1, double y1, double x2, double y2)
+{
+    std::vector<std::vector<double>> new_points;
+    int new_poly_size = 0;
+  
+    // (ix,iy),(kx,ky) are the co-ordinate values of
+    // the points
+    for (int i = 0; i < poly_size; i++)
+    {
+        // i and k form a line in polygon
+        int k = (i+1) % poly_size;
+        int ix = poly_points[i][0], iy = poly_points[i][1];
+        int kx = poly_points[k][0], ky = poly_points[k][1];
+  
+        // Calculating position of first point
+        // w.r.t. clipper line
+        int i_pos = (x2-x1) * (iy-y1) - (y2-y1) * (ix-x1);
+  
+        // Calculating position of second point
+        // w.r.t. clipper line
+        int k_pos = (x2-x1) * (ky-y1) - (y2-y1) * (kx-x1);
+  
+        // Case 1 : When both points are inside
+        if (i_pos < 0  && k_pos < 0)
+        {
+            //Only second point is added
+            new_points[new_poly_size][0] = kx;
+            new_points[new_poly_size][1] = ky;
+            new_poly_size++;
+        }
+  
+        // Case 2: When only first point is outside
+        else if (i_pos >= 0  && k_pos < 0)
+        {
+            // Point of intersection with edge
+            // and the second point is added
+            new_points[new_poly_size][0] = x_intersect(x1,
+                              y1, x2, y2, ix, iy, kx, ky);
+            new_points[new_poly_size][1] = y_intersect(x1,
+                              y1, x2, y2, ix, iy, kx, ky);
+            new_poly_size++;
+  
+            new_points[new_poly_size][0] = kx;
+            new_points[new_poly_size][1] = ky;
+            new_poly_size++;
+        }
+  
+        // Case 3: When only second point is outside
+        else if (i_pos < 0  && k_pos >= 0)
+        {
+            //Only point of intersection with edge is added
+            new_points[new_poly_size][0] = x_intersect(x1,
+                              y1, x2, y2, ix, iy, kx, ky);
+            new_points[new_poly_size][1] = y_intersect(x1,
+                              y1, x2, y2, ix, iy, kx, ky);
+            new_poly_size++;
+        }
+  
+        // Case 4: When both points are outside
+        else
+        {
+            //No points are added
+        }
+    }
+  
+    // Copying new points into original array
+    // and changing the no. of vertices
+    poly_size = new_poly_size;
+    for (int i = 0; i < poly_size; i++)
+    {
+        poly_points[i][0] = new_points[i][0];
+        poly_points[i][1] = new_points[i][1];
+    }
+}
+
+// Implement Sutherland–Hodgman algorithm
+void ca::LawnMowerPattern::suthHodgClip(std::vector<std::vector<double>> poly_points, std::vector<std::vector<double>> clipper_points)
+{
+    //i and k are two consecutive indexes
+    for (int i=0; i<clipper_points.size(); i++)
+    {
+        int k = (i+1) % clipper_points.size();
+  
+        // We pass the current array of vertices, it's size
+        // and the end points of the selected clipper line
+        clip(poly_points, poly_points.size(), clipper_points[i][0],
+             clipper_points[i][1], clipper_points[k][0],
+             clipper_points[k][1]);
+    }
+  
+    // Printing vertices of clipped polygon
+    for (int i=0; i < poly_points.size(); i++)
+        std::cout << '(' << poly_points[i][0] <<
+                ", " << poly_points[i][1] << ") ";
+}
+
+void ca::LawnMowerPattern::cellDecomposition(std::vector<std::vector<double>> boundary_polygon, std::vector<std::vector<std::vector<double>>> obstacle_polygons, double row_distance){
+    double x,y,heading;
+    x=y=heading=0;
+    _path_x_y_heading.clear();
+
+    /* sort the boundary and obstacle polygons by x-coords */
+    // identify boundary coords by 0  
+    for (int i=0; i<boundary_polygon.size(); ++i)
+        boundary_polygon[i].push_back(0.0);
+    
+    // identify obstacle coords by 1
+    for (int i=0; i<obstacle_polygons.size(); ++i)
+    {
+        for (int j=0; j<obstacle_polygons[i].size(); ++j)
+            obstacle_polygons[i][j].push_back(1.0);
+    }
+    // copy the boundary polygon into a new vector for manipulation
+    auto ordered_coords = boundary_polygon;
+    
+    // first flatten obstacle polygon vector into a 2D vector of coords in boundary polygon vector
+    for (int i=0; i<obstacle_polygons.size(); ++i)
+        ordered_coords.insert(ordered_coords.end(), obstacle_polygons[i].begin(), obstacle_polygons[i].end());
+
+
+    // sort 2D vector based on x-coord in ascending order
+    std::sort(ordered_coords.begin(), ordered_coords.end(),
+          [](const std::vector<double>& a, const std::vector<double>& b) {
+                    return a[0] < b[0];
+                    });
+    
+    /* trapezoidal cell decomposition */
+    // iterate over ordered_coords and draw a vertical line through every coord
+    std::vector<std::vector<double>> cell_coords; 
+    std::vector<std::vector<std::vector<double>>> cells;
+    std::vector<std::vector<double>> coord_backlog;
+    // let's assume for now its a convex polygon
+    // we leave out the first and last coord while iterating
+    for (int i=1; i<ordered_coords.size(); ++i)
+    {
+        // find intersects with boundary of polygon
+        std::vector<std::vector<double>> intersects = intersectBoundary(boundary_polygon, ordered_coords[i]);
+        if (i==1)
+        {
+            cell_coords.push_back(ordered_coords[i-1]);
+            for (int i=0; i<intersects.size(); ++i)
+                cell_coords.push_back(intersects[i]);
+        }
+        else if (i==ordered_coords.size()-1)
+        {
+            int intersect_count = 0;
+            for (int j=coord_backlog.size(); j>0; --j)
+            {
+                if (intersect_count==2)
+                    break;
+                else
+                {
+                    cell_coords.push_back(coord_backlog[i]);
+                    if (coord_backlog[i][2] == -1)
+                        ++intersect_count;
+                }
+            }
+            for (int i=0; i<intersects.size(); ++i)
+                cell_coords.push_back(intersects[i]);
+        }
+        else
+        {
+            int intersect_count = 0;
+            for (int j=coord_backlog.size(); j>0; --j)
+            {
+                if (intersect_count==2)
+                    break;
+                else
+                {
+                    cell_coords.push_back(coord_backlog[i]);
+                    if (coord_backlog[i][2] == -1)
+                        ++intersect_count;
+                }
+            }
+            for (int i=0; i<intersects.size(); ++i)
+                cell_coords.push_back(intersects[i]);
+        }
+        // check if current_coord is boundary point or obstacle point
+        // find up line and down line
+        // if boundary coord, check if up/down line in boundary or not
+        // if obstacle coord, check if up/down line in obstacle or not
+        // if not in obstacle / if not outside bounds then
+        // find intersect of line with line segment formed by closest coord
+        // to do this, find all closest coords (behind the coord in ordered_coord)
+        // for each, check if y < current_coord_y
+        // if less, segment coord is behind
+        // if more, segment coord is ahead
+        // once intersect obtained, status of current coord is changed to open, coords behind changed to closed
+        // store bounds of closed cell and index of closed cell
+
+        coord_backlog = cell_coords;
+        cells.push_back(cell_coords);
+    }
+
+    // find obstacle intersects and build adjacency graph
+    std::vector<std::vector<int>> cell_adjacency_graph;
+    for (int i=0; i<cells.size(); ++i)
+    {
+        for (int j=0; j<obstacle_polygons.size(); ++j)
+        {
+            // cell is clipper, obstacle is polygon
+            suthHodgClip(obstacle_polygons[j], cells[i]);
+        }
+    }
+    
+}
+
 
 bool ca::LawnMowerPattern::GenerateLawnMowerPattern(std::vector<LawnMowerPoint> & path){
     path.clear();
